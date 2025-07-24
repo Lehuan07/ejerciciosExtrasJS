1d5d2a2769260d5bc5e24cb633d1d1a4c5dff013
Nahuel C. <nahuelncejas@gmail.com>
2025-07-23 22:47:21 -0300

    EJERCICIO 3: Lista de tareas con marcado de completado
    <pre>Objetivo: <br>
    Crear una lista de tareas donde cada tarea pueda marcarse como completada (tachada o con estilo <br>
    distinto) y también pueda eliminarse.</pre>

---
+++ Ejercicio3ListaDeTareasConMarcadoDeCompletado/script.js
@@ -0,0 +1,10 @@
+/*
+🧠 Requisitos funcionales:
+Ingresar una tarea en el campo de texto y agregarla con un botón.
+Cada tarea debe aparecer como un <li> en la lista.
+Cada <li> debe tener:
+El texto de la tarea.
+Un botón ❌ para eliminarla.
+Si se hace clic sobre el texto de la tarea (no el botón), la tarea se marca como completada:
+Puede ser tachada (con CSS: text-decoration: line-through) o con otro estilo que elijas.
+*/
