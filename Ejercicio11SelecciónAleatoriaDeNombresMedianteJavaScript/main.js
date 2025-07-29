7f604d558193bd45b61812e6565c5af513a15443
Nahuel C. <nahuelncejas@gmail.com>
2025-07-28 22:28:18 -0300

    Empiezo el ejercicio 11

---
+++ Ejercicio11SelecciónAleatoriaDeNombresMedianteJavaScript/main.js
@@ -0,0 +1,6 @@
+/*
+Crea un evento que se active al hacer clic en el botón «Agregar». Este evento debe llamar a una función que obtenga el valor del input, agregue el nombre a una lista en memoria y luego actualice la lista visible en el DOM.
+Crea una función para renderizar los nombres en la lista. Esta función debe actualizar el contenido del elemento <ul> con los nombres almacenados en la lista en memoria.
+Crea un evento que se active al hacer clic en el botón «Seleccionar aleatoriamente». Esta función debe seleccionar aleatoriamente un nombre de la lista en memoria y resaltarlo en la lista visible en el DOM. Además, se debe mostrar un mensaje indicando que se ha elegido ese nombre.
+Verifica que el programa funcione correctamente. Al hacer clic en el botón «Seleccionar aleatoriamente», debe aparecer un nombre resaltado en la lista con el mensaje indicando que ha sido seleccionado aleatoriamente.
+*/
